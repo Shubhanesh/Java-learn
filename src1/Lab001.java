c13f163deef56e0afffb4c6de9b2e2a48d6e815f
Shubhanesh <shubhanesh1994@gmail.com>
2023-12-24 16:49:56 +0530

    first commit

---
+++ src1/Lab001.java
@@ -0,0 +1,25 @@
+package src1;
+
+public class Lab001 {
+    public static void main(String[] args) {
+      //int a=98;
+      //switch(a)
+      //  {
+            //case 98:
+               // System.out.println("Value-98");
+            //case 98:
+                //System.out.println("value -98");
+
+       // }
+        byte b=20;
+        switch (b) {
+            case 20:
+                System.out.println("TEN");
+            case 127:
+                System.out.println("One Twenty ");
+
+            default:
+                System.out.println("other number");
+        }
+    }
+}
